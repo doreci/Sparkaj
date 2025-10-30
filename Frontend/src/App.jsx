ccfb395c05ed339e2650cfc750b573af5a52bcaa
Karlo Piškorić <81332948+piskoric@users.noreply.github.com>
2025-10-30 18:11:06 +0100

    Dodavanje responzivnosti

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import './App.css'
-import Login from './components/login'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import "./App.css";
+import Login from "./components/login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = "1037227751449-vnk1ihmcvnbje5sq3b6e67u4o1klfqrv.apps.googleusercontent.com";
+const clientId =
+    "1037227751449-vnk1ihmcvnbje5sq3b6e67u4o1klfqrv.apps.googleusercontent.com";
 
 function App() {
-  return (
-    <GoogleOAuthProvider clientId={clientId}>
-        <div className="background">
-              <img src="/background.jpg"  alt="" />
-              <div className="overlay"></div>
-              <div className="logo">
-                  <img src="/logozad.png" alt="" />
-              </div>
-        <div className="login-container">
-          <Login />
-        </div>
-    </div>
-    </GoogleOAuthProvider>
-  )
+    return (
+        <GoogleOAuthProvider clientId={clientId}>
+            <div className="background">
+                <img src="/background.jpg" alt="" />
+            </div>
+            <div className="center">
+                <div className="logo">
+                    <img src="/logo.png" alt="logo.png" />
+                </div>
+                <div className="login-container">
+                    <Login />
+                </div>
+            </div>
+        </GoogleOAuthProvider>
+    );
 }
 
-export default App
+export default App;
