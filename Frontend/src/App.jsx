698c6c6e00bcff283f641f7be64398529fde30f9
Dorian Ceci <dorian.ceci@fer.hr>
2025-11-03 21:33:18 +0100

    Dodan privremeni OAuth 2.0 servis #4

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,62 @@
+import { supabase } from "../supabaseClient";
 import "./App.css";
 import Login from "./components/login";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useEffect, useState } from "react";
 
 const clientId =
     "1037227751449-vnk1ihmcvnbje5sq3b6e67u4o1klfqrv.apps.googleusercontent.com";
 
 function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    return () => subscription.unsubscribe()
+  }, []);
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+  };
+
+  if (!session) {
     return (
+      <>
         <GoogleOAuthProvider clientId={clientId}>
-            <div className="background">
-                <img src="/background.jpg" alt="" />
-            </div>
-            <div className="center">
-                <div className="logo">
-                    <img src="/logo.png" alt="logo.png" />
-                </div>
-                <div className="login-container">
-                    <Login />
-                </div>
-            </div>
+          <div className="background">
+              <img src="/background.jpg" alt="" />
+          </div>
+          <div className="center">
+              <div className="logo">
+                  <img src="/logo.png" alt="logo.png" />
+              </div>
+              <div className="login-container">
+                  <Login />
+              </div>
+          </div>
         </GoogleOAuthProvider>
+      </>
     );
+  }
+  else {
+      return (
+        <>
+          <div>
+            <h2>Dobrodošao/la, {session?.user?.user_metadata?.full_name}</h2>
+            <button onClick={signOut}>Sign out</button>
+          </div>
+        </>
+    );
+  }
 }
 
 export default App;
