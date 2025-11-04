879acd29f19472e711c4e34d24f01583a4cc2fb9
Karlo Piškorić <81332948+piskoric@users.noreply.github.com>
2025-11-04 01:11:17 +0100

    Dodavanje početne stranice #1

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,10 +1,27 @@
-
 import "./homepage.css";
+import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
-        <div className="dobrodosli">DOBRODOSLI</div>
-    )
+        <div className="container">
+            <div className="header">
+                <img src="/logo.png" alt="logo" />
+                <div className="header-buttons">
+                    <Link to="/register">
+                        <button>Register</button>
+                    </Link>
+                    <Link to="/login">
+                        <button>Login</button>
+                    </Link>
+                </div>
+            </div>
+            <div className="content">
+                <h1>Welcome to Sparkaj</h1>
+            </div>
+            <div className="footer">
+                <p>&copy; 2025 Sparkaj. All rights reserved.</p>
+            </div>
+        </div>
+    );
 }
-
 export default HomePage;
