36b9037e80e185564e524e224ab12a9e5337ec09
Dorian Ceci <dorian.ceci@fer.hr>
2026-01-10 19:30:49 +0100

    Fix nazivlja

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/editprofile" element={<EditProfilePage/>} />
-        <Route path="/parking-spots" element={<ParkingSpotsPage />} />
+        <Route path="/oglasi" element={<ParkingSpotsPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
